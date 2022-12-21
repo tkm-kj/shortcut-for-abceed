bd5c36673d8aac3d7410ea30a15322bad7878fdc
Takumi Kaji <ivezuki3@gmail.com>
2022-12-21 13:46:11 +0900

    refactor

--- content_script.js
+++ content_script.js
@@ -5,19 +5,19 @@ document.addEventListener('keydown', (event) => {
   if (keysPressed['Shift']) {
     switch (event.code) {
       case 'ArrowLeft':
-        pushButton(1)
+        pushSoundControllerButton(1)
         break
       case 'ArrowRight':
-        pushButton(3)
+        pushSoundControllerButton(3)
         break
       case 'ArrowUp':
-        changeSelectBox(-1)
+        changeVoiceSpeedSelectBox(-1)
         break
       case 'ArrowDown':
-        changeSelectBox(1)
+        changeVoiceSpeedSelectBox(1)
         break
       case 'Space':
-        pushButton(2)
+        pushSoundControllerButton(2)
         break
     }
   }
@@ -27,16 +27,16 @@ document.addEventListener('keyup', (event) => {
   delete keysPressed[event.key]
 })
 
-function pushButton(index) {
+function pushSoundControllerButton(index) {
   const els = document.getElementsByClassName('sound-controller_item')
   els[index].click()
 }
 
-function changeSelectBox(diff) {
+function changeVoiceSpeedSelectBox(diff) {
   const currentSpeed = document.getElementsByClassName('current-rate')[0].innerText
   const selectEl = document.querySelector('select.playback-rate_select')
   const options = selectEl.options
-  currentIndex = Array.from(options).findIndex(option => Number.parseFloat(option.value) === Number.parseFloat(currentSpeed.replace('×', '')))
+  const currentIndex = Array.from(options).findIndex(option => Number.parseFloat(option.value) === Number.parseFloat(currentSpeed.replace('×', '')))
   const newIndex = currentIndex + diff
   if (newIndex >= 0 && newIndex <= options.length - 1) {
     selectEl.selectedIndex = newIndex
